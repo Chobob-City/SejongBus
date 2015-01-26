47fc151701ff2ac7aed917cdfd3d083721f7325d
육진혁 <jdd04026@gmail.com>
2015-01-26 23:14:23 +0900

    no message

--- app/src/main/java/kr/nogcha/sejongbus/MainActivity.java
+++ app/src/main/java/kr/nogcha/sejongbus/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
