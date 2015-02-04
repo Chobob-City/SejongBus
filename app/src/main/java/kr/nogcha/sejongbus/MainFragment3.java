11c48289b3dad345ea33b72bce2fc63efaeb861f
Kang Hyeonwoo <kanglib@naver.com>
2015-02-04 15:59:55 +0900

    Map patch #2

--- app/src/main/java/kr/nogcha/sejongbus/MainFragment3.java
+++ app/src/main/java/kr/nogcha/sejongbus/MainFragment3.java
@@ -17,84 +17,15 @@
 package kr.nogcha.sejongbus;
 
 import android.app.Fragment;
-import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-
-public class MainFragment3 extends Fragment implements GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener, LocationListener {
-    private GoogleApiClient mApiClient;
-    private LocationRequest mLocationRequest;
-    private Location mCurrentLocation;
-    private TextView textView;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        initializeGoogleApi();
-        mApiClient.connect();
-    }
-
+public class MainFragment3 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.f_main_3, container, false);
-
-        textView = (TextView) rootView.findViewById(R.id.textView);
-
-        return rootView;
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (mApiClient.isConnected()) mApiClient.disconnect();
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(mApiClient);
-        if (lastLocation != null) {
-            textView.setText("lat=" + lastLocation.getLatitude() + "&lng="
-                    + lastLocation.getLongitude());
-        } else {
-            Toast.makeText(getActivity(), "lastLocation is null", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        mApiClient.connect();
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.e("MainFragment3", "Connection failed (" + connectionResult.getErrorCode() + ")");
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-    }
-
-    private synchronized void initializeGoogleApi() {
-        mApiClient = new GoogleApiClient.Builder(getActivity()).addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
-
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(3000);
-        mLocationRequest.setFastestInterval(1500);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return inflater.inflate(R.layout.f_main_3, container, false);
     }
 }
