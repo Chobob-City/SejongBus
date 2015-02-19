f643a89d08e4ff3587df20b85368b2e534be9d5a
Kang Hyeonwoo <kanglib@naver.com>
2015-02-20 00:50:12 +0900

    1h build

--- app/src/main/java/kr/nogcha/sejongbus/main/SurroundStopFragment.java
+++ app/src/main/java/kr/nogcha/sejongbus/main/SurroundStopFragment.java
@@ -23,6 +23,7 @@ import android.content.Intent;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.SpannableString;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,7 +34,10 @@ import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -54,30 +58,23 @@ import kr.nogcha.sejongbus.R;
 import kr.nogcha.sejongbus.SejongBisClient;
 
 public class SurroundStopFragment extends Fragment implements GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback {
-    private ArrayList<CommonListItem> mList = new ArrayList<>();
-    private Location mLastLocation = null;
+        LocationListener {
     private GoogleApiClient mApiClient;
-    private SejongBisClient mBisClient;
-    private CommonAdapter mAdapter;
+    private GoogleMap mMap;
+    private ListView mListView;
     private JSONArray mJSONArray;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Activity activity = getActivity();
-        mApiClient = new GoogleApiClient.Builder(activity).addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
-        mBisClient = new SejongBisClient(activity);
-        mAdapter = new CommonAdapter(activity, R.layout.common_list_item, mList);
+        mApiClient = new GoogleApiClient.Builder(getActivity()).addConnectionCallbacks(this)
+                .addApi(LocationServices.API).build();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.f_surround_stop, container, false);
-
-        MainActivity.hideSoftInput();
 
         FragmentManager fragmentManager;
         if (Build.VERSION.SDK_INT >= 17) {
@@ -86,25 +83,18 @@ public class SurroundStopFragment extends Fragment implements GoogleApiClient.Co
             fragmentManager = getFragmentManager();
         }
         MapFragment map = (MapFragment) fragmentManager.findFragmentById(R.id.map);
-        map.getMapAsync(this);
-
-        ListView listView = (ListView) rootView.findViewById(R.id.list_view);
-        listView.setEmptyView(rootView.findViewById(R.id.text_view));
-        listView.setAdapter(mAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        map.getMapAsync(new OnMapReadyCallback() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getActivity(), BusStopRouteActivity.class);
-                Bundle bundle = new Bundle();
-                try {
-                    bundle.putInt("stop_id", mJSONArray.getJSONObject(position).getInt("stop_id"));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                intent.putExtras(bundle);
-                startActivity(intent);
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
+                mMap.setMyLocationEnabled(true);
             }
         });
+
+        MainActivity.hideSoftInput();
+
+        mListView = (ListView) rootView.findViewById(R.id.list_view);
+        mListView.setEmptyView(rootView.findViewById(R.id.text_view));
 
         return rootView;
     }
@@ -120,9 +110,12 @@ public class SurroundStopFragment extends Fragment implements GoogleApiClient.Co
         super.onResume();
         Activity activity = getActivity();
         int errorCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(activity);
-        if (errorCode != ConnectionResult.SUCCESS
-                && !GooglePlayServicesUtil.isUserRecoverableError(errorCode)) {
-            Toast.makeText(activity, "해당 기기는 지원되지 않습니다.", Toast.LENGTH_SHORT).show();
+        if (errorCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(errorCode)) {
+                GooglePlayServicesUtil.getErrorDialog(errorCode, activity, 0).show();
+            } else {
+                Toast.makeText(activity, "해당 기기는 지원되지 않습니다.", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
@@ -134,10 +127,12 @@ public class SurroundStopFragment extends Fragment implements GoogleApiClient.Co
 
     @Override
     public void onConnected(Bundle bundle) {
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mApiClient);
-        if (mLastLocation == null) {
-            Toast.makeText(getActivity(), "위치를 사용할 수 없습니다.", Toast.LENGTH_SHORT).show();
-        }
+        LocationRequest request = new LocationRequest();
+        request.setInterval(10000);
+        request.setFastestInterval(5000);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setNumUpdates(1);
+        LocationServices.FusedLocationApi.requestLocationUpdates(mApiClient, request, this);
     }
 
     @Override
@@ -146,36 +141,51 @@ public class SurroundStopFragment extends Fragment implements GoogleApiClient.Co
     }
 
     @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-    }
+    public void onLocationChanged(Location location) {
+        if (mMap == null) return;
 
-    @Override
-    public void onMapReady(final GoogleMap googleMap) {
-        //noinspection StatementWithEmptyBody
-        while (mLastLocation == null) ;
+        Activity activity = getActivity();
+        SejongBisClient bisClient = new SejongBisClient(activity);
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
         try {
-            mJSONArray = mBisClient
-                    .searchSurroundStopList(mLastLocation.getLatitude(),
-                            mLastLocation.getLongitude())
+            mJSONArray = bisClient.searchSurroundStopList(latitude, longitude)
                     .getJSONArray("busStopList");
-            googleMap.clear();
-            mList.clear();
+            mMap.clear();
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(latitude, longitude), (float) 14.5));
+            ArrayList<CommonListItem> list = new ArrayList<>();
             for (int i = 0; i < mJSONArray.length(); i++) {
                 CommonListItem item = new CommonListItem();
                 JSONObject json = mJSONArray.getJSONObject(i);
-
+                item.text1 = new SpannableString("");
                 item.text2 = json.getString("stop_name");
                 item.text3 = json.getString("service_id");
-                googleMap.addMarker(new MarkerOptions()
+                mMap.addMarker(new MarkerOptions()
                         .position(new LatLng(json.getDouble("lat"), json.getDouble("lng")))
                         .title(item.text2 + " [" + item.text3 + "]"));
-
                 item.text3 += "\n" + json.getString("distance") + "m";
-                mList.add(item);
+                list.add(item);
             }
+
+            mListView.setAdapter(new CommonAdapter(activity, R.layout.common_list_item, list));
+            mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent intent = new Intent(getActivity(), BusStopRouteActivity.class);
+                    Bundle bundle = new Bundle();
+                    try {
+                        bundle.putInt("stop_id",
+                                mJSONArray.getJSONObject(position).getInt("stop_id"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+                }
+            });
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        mAdapter.notifyDataSetChanged();
     }
 }
